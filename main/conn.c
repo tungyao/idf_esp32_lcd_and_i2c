5c79f46a9beaa1baa70bc36db2f08a03045ed865
tungyao <tungyao@qq.com>
2024-07-18 09:42:59 +0800

    fix wifi_config_t init

--- main/conn.c
+++ main/conn.c
@@ -172,16 +172,13 @@ void wifi_init_sta(char *ssid, char *pwd) {
 
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = "redmi12345678",
-            .password = "123123123123",
-            /* Authmode threshold resets to WPA2 as default if password matches WPA2 standards (pasword len => 8).
-             * If you want to connect the device to deprecated WEP/WPA networks, Please set the threshold value
-             * to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK and set the password with length and format matching to
-             * WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK standards.
-             */
             .threshold.authmode = WIFI_AUTH_WPA2_PSK,
         },
     };
+    size_t ssid_length = strlen(ssid);
+    size_t pwd_length = strlen(pwd);
+    memcpy(wifi_config.sta.ssid, ssid, ssid_length);
+    memcpy(wifi_config.sta.password, pwd, pwd_length);
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
@@ -276,61 +273,6 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
             break;
     }
 }
-
-
-// int tcp_client(void) {
-//     char rx_buffer[256];
-//     char host_ip[] = HOST_IP_ADDR;
-//     int addr_family = 0;
-//     int ip_protocol = 0;
-//     char *payload = "weather:chongqing";
-//     struct sockaddr_in dest_addr;
-//     inet_pton(AF_INET, host_ip, &dest_addr.sin_addr);
-//     dest_addr.sin_family = AF_INET;
-//     dest_addr.sin_port = PORT;
-//     addr_family = AF_INET;
-//     ip_protocol = IPPROTO_IP;
-//
-//     int sock = socket(addr_family, SOCK_STREAM, ip_protocol);
-//     if (sock < 0) {
-//         ESP_LOGE(CONN_TAG, "Unable to create socket: errno %d", sock);
-//         return 0;
-//     }
-//     ESP_LOGI(CONN_TAG, "Socket created, connecting to %s:%d", host_ip, PORT);
-//
-//     int err = connect(sock, (struct sockaddr *) &dest_addr, sizeof(dest_addr));
-//     if (err != 0) {
-//         ESP_LOGE(CONN_TAG, "Socket unable to connect: errno %d", err);
-//         return 0;
-//     }
-//     ESP_LOGI(CONN_TAG, "Successfully connected");
-//
-//     while (1) {
-//         int err = send(sock, payload, strlen(payload), 0);
-//         if (err < 0) {
-//             ESP_LOGE(CONN_TAG, "Error occurred during sending: errno %d", err);
-//             break;
-//         }
-//
-//         int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-//         // Error occurred during receiving
-//         if (len < 0) {
-//             ESP_LOGE(CONN_TAG, "recv failed: errno %d", errno);
-//             break;
-//         }else {
-//             rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
-//             ESP_LOGI(CONN_TAG, "Received %d bytes from %s:", len, host_ip);
-//             ESP_LOGI(CONN_TAG, "%s", rx_buffer);
-//             break;
-//         }
-//     }
-//
-//     ESP_LOGE(CONN_TAG, "Shutting down socket and restarting...");
-//     shutdown(sock, 0);
-//     close(sock);
-//     return 1;
-// }
-
 
 int tcp_client2(void) {
     tcp_client_t client;
